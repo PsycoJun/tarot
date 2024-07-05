c04c72b1b0bf077d6e928d68862842900d6eb91e
baskduf <baskduf@gmail.com>
2024-07-05 11:05:37 +0900

    UIã작업 및 스í정적 파일 추가

---
+++ static/button_scripts.js
@@ -0,0 +1,4 @@
+
+document.querySelectorAll('.button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
+//# sourceURL=pen.js
+
