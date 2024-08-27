d94ddba1483cb12333c2607e51d56343dd3f0958
IsoletEXT <jsm020710@gmail.com>
2024-08-27 22:16:55 +0900

    fix: settings.py 파일 데이터베이스 수정

--- tarot_project/settings.py
+++ tarot_project/settings.py
@@ -76,8 +76,7 @@ WSGI_APPLICATION = 'tarot_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
 
